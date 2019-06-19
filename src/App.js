a191f2735338f22932679a56c6c9c42ef1d7caa7
CesarRivasP <cesarjose58@gmail.com>
2019-06-18 20:27:59 -0400

    implemented quote component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './assets/logo.svg';
+import Quote from './components/quote';
 
 function App() {
-  return (
-    <div className="">
 
+  const [quote, getQuote] = useState({});
+
+  // consultar api
+  const consultAPI = async() => {
+    const result = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+    // agregar el resultado de la api al state
+    getQuote(result.data[0]);
+  }
+
+  // consulta a una rest api. Siempre se utiliza useEffect
+  useEffect(() => {
+    consultAPI();
+  }, []);
+
+  return (
+    <div className="contenedor">
+      <img className="logo-container" src={logo} alt="breaking bad logo" width="400px" height="400px"/>
+      <Quote quote={quote} />
     </div>
   );
 }
